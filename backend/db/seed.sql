0026288058f670428ae66975dfb399236a556f62
Erick <erickarellano@pursuit.org>
2019-04-10 12:51:27 -0400

    some data in the seed

--- backend/db/seed.sql
+++ backend/db/seed.sql
@@ -14,6 +14,7 @@ CREATE TABLE landlords (
 
 CREATE TABLE apartments (
     id SERIAL PRIMARY KEY,
+    name VARCHAR NOT NULL,
     address VARCHAR NOT NULL,
     landlord_id INT NOT NULL REFERENCES landlords(id)
 );
@@ -23,6 +24,7 @@ CREATE TABLE tenants(
     name VARCHAR NOT NULL, 
     dob DATE, 
     email VARCHAR NOT NULL,
+    phone VARCHAR NOT NULL,
     apartment_id INT NOT NULL REFERENCES apartments(id),
     password_digest VARCHAR NOT NULL
 );
@@ -32,10 +34,27 @@ CREATE TABLE tickets(
     apartment_id INT NOT NULL REFERENCES apartments(id),
     subject VARCHAR NOT NULL,
     body TEXT NOT NULL,
-    status BOOLEAN,
+    status BIT,
     appt_date DATE NOT NULL,
     appt_time TIME NOT NULL
 );
 
--- INSERT INTO landlords(name, email, phone, dob, password_digest) VALUES ('James Bennett', 'jBennet@gmail.com', '4582019297', )
+INSERT INTO landlords(name, email, phone, dob, password_digest) VALUES ('James Bennett', 'jBennet@gmail.com', '(458)201-9297', '1992-01-01', 'abc123' ),
+                                                                        ('Rick Nixon', 'rNixon@gmail.com', '(321)861-7901', '1990-02-02', '123abc' ),
+                                                                        ('Yasmin Henry', 'yHenry@gmail.com', '(434)847-4706', '1992-03-03', '456xyz' ),
+                                                                        ('Reya Herbert', 'rHerbert@gmail.com', '(562)298-3280', '1992-04-05', 'xyz456' ),
+                                                                        ('Sol Finney', 'sFinney@gmail.com', '(402)899-1372', '1992-01-02', '987abc' );
 
+INSERT INTO apartments(name, address, landlord_id) VALUES ('4C', '939 Woodycrest Ave, Bronx, NY 10452', 1),
+                                                     ('3', '5959 Broadway, Bronx, NY 10463', 2),
+                                                     ('10', '250 Bedford Park Blvd, Bronx, NY 10458', 3),
+                                                     ('1A', '287 Onderdonk Ave, Queens, NY 11385', 4),
+                                                     ('5D', '16-74 Bell Blvd, Queens, NY 11360', 5);
+
+INSERT INTO tenants(name, email, phone, dob, apartment_id, password_digest) VALUES ('Zoe Goulding', 'zGoulding@gmail.com', '(609)367-5162', '1998-10-01', '1',  'abc123' ),
+                                                                                    ('Nikola Ryder', 'nRyder@gmail.com', '(319)471-7363', '1995-12-02', '2',  '123abc' ),
+                                                                                    ('Kylie Thomas', 'kThomas@gmail.com', '(440)685-5383', '1996-06-03', '3', '456xyz' ),
+                                                                                    ('Brody Welsh', 'bWelsh@gmail.com', '(312)600-6147', '1991-04-05', '4',  'xyz456' ),
+                                                                                    ('Karol Lu', 'kLu@gmail.com', '(206)213-6903', '1980-09-02', '5', '987abc' );
+
+INSERT INTO tickets(apartment_id, subject, body, status, appt_date, appt_time) VALUES (1, 'Plumbing', 'Bathroom sink not working', '0', '2019-05-05', '12:00 [PM]' );
