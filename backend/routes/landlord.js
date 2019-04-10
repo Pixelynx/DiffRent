69aff3d97a8f9d62c29818ce1a10ff9d6dd64dce
pixelynx <tjstevens1992@gmail.com>
2019-04-10 14:56:58 -0400

    get single landlord router

--- backend/routes/landlord.js
+++ backend/routes/landlord.js
@@ -3,8 +3,10 @@ const router = express.Router();
 const queries = require('../db/queries/landlords.js');
 
 router.get('/', queries.getAllLandlords);
+router.get('/:id', queries.getSingleLandlord);
 router.post('/', queries.addNewLandlord);
 router.delete('/:id', queries.deleteLandlord);
 router.put('/:id', queries.updateLandlord);
+
 
 module.exports = router;
