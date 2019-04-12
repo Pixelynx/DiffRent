221259aef78dde04279d5acf0308395ea7948ac1
Erick <erickarellano@pursuit.org>
2019-04-12 12:15:52 -0400

    frontend created

---
+++ backend/client/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h3>Team 3!</h3>
+      </div>
+    );
+  }
+}
+
+export default App;
