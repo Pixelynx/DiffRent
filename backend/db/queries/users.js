33ff3352053ee8edb849fad030fc99902cc20bfd
Erick <erickarellano@pursuit.org>
2019-04-17 11:28:22 -0400

    badass query

--- backend/db/queries/users.js
+++ backend/db/queries/users.js
@@ -1,5 +1,22 @@
 const db = require('../../db/index.js');
 
+const getSingleUser = (req, res, next) => {
+    email= req.params.id;
+  db.one("SELECT users.id AS userId, name, email, phone, dob, user_type, apartments.id AS aptId, apt, address, tenant_id, COUNT(tickets.id) AS openTickets FROM users JOIN apartments ON (users.id = apartments.landlord_id OR users.id = apartments.tenant_id) JOIN tickets ON (apartments.id = tickets.apartment_id AND (completed_landlord='0' OR completed_tenant='0')) WHERE users.email=$1 GROUP BY (users.id, apartments.id)", email)
+    .then(data => {
+      res.status(200)
+         .json({
+           status: "Success",
+           data: data,
+           message: "Received Single User"
+         })
+    })
+    .catch(err => {
+      console.log("error: ", err)
+      next(err)
+    })
+}
+
 const logoutUser = (req, res, next) => {
     req.logout();
     res.status(200).send("log out success");
@@ -14,6 +31,7 @@ const isLoggedIn = (req, res) => {
 }
 
 module.exports = {
+    getSingleUser: getSingleUser,
     logoutUser: logoutUser,
     isLoggedIn: isLoggedIn
   }
