8d1168f6420e1b98c04a85039315c5252ec97d3b
Kelly <kellyliang95@gmail.com>
2019-04-17 00:00:20 -0400

    created threads queries

--- backend/routes/messages.js
+++ backend/routes/messages.js
@@ -2,11 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const {
-  getAllMsgs,
   addMsg
 } = require("../db/queries/messages");
 
-router.get('/allmessages', getAllMsgs);
 router.post('/newmessage', addMsg);
 
 module.exports = router;
