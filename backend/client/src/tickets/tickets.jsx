6c5159d5d052f7f989e1c9e89dfd08320e4c4e06
pixelynx <tjstevens1992@gmail.com>
2019-04-17 15:11:31 -0400

    DON'T CLICK 'CREATE TICKET'

--- backend/client/src/tickets/tickets.jsx
+++ backend/client/src/tickets/tickets.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import TenantTicket from './tenantTickets/tenantTicket.jsx';
+import CreateTicketForm from './tenantTickets/createTicketForm.jsx';
 import '../styles/colorScheme.css';
 import '../styles/tickets/tickets.css';
 
@@ -8,6 +9,7 @@ class Tickets extends Component {
   constructor() {
     super()
   }
+
   state = {
     apt: 2,
     ticketModalOpen: false,
@@ -16,6 +18,7 @@ class Tickets extends Component {
     landlordMarkedInProgress: false,
     landlordMarkedResolved: false,
     tenantMarkedResolved: false,
+    creatingTicket: false
   }
 
   componentDidMount = () => {
@@ -43,38 +46,44 @@ class Tickets extends Component {
     this.setState({ tenantMarkedResolved: !currentState })
   }
 
+  handleCreateTicketBtn = (e) => {
+    this.setState({ creatingTicket: true })
+    this.setState({ ticketModalOpen: false })
+
+  }
+
   displayUnresolvedTickets = () => {
     // need to change up ticket resolve -- append resolve to tenants and landlord tickets
     const { ticketsUnresolved, ticketsResolved, ticketModalOpen } = this.state
     let status = 'RESOLVE';
     if(ticketsUnresolved || ticketsResolved && ticketModalOpen) {
-      let showTickets = ticketsUnresolved.map(ticket => {
+      return ticketsUnresolved.map(ticket => {
         let date = ticket.appt_date
         let apptDate = new Intl.DateTimeFormat('en-US').format(new Date(date))
             if(!ticket.status) status = 'RESOLVED';
           return(
             <>
-            <div className='ticket-window'>
-              <ul className='ticket-front'>
-                <li className='ticket-item' id='ticket-subject-front'>Issue: {ticket.subject}</li>
-                <li className='ticket-item' id='appt-date-time-front'>Appointment: {apptDate} {ticket.appt_time}</li>
-                <li>{ticket.status ? 'UNRESOLVED' : 'RESOLVED'}</li>
-              </ul>
-              <ul className='ticket-back'>
-                <li className='ticket-item' id='ticket-subject-back'>Issue: {ticket.subject}</li>
-                <li className='ticket-item' id='appt-date-time-back'>Appointment: {apptDate} {ticket.appt_time}</li>
-                <li className='ticket-item' id='ticket-desc'>Description: {ticket.body}</li>
+            <div key={ticket.id} className='ticket-window'>
+              <div className='ticket-front'>
+                <p className='ticket-item' id='ticket-subject-front'>Issue: {ticket.subject}</p>
+                <p className='ticket-item' id='appt-date-time-front'>Appointment: {apptDate} {ticket.appt_time}</p>
+                <p>{ticket.status ? 'UNRESOLVED' : 'RESOLVED'}</p>
+              </div>
+              <div key={ticket.id} className='ticket-back'>
+                <p className='ticket-item' id='ticket-subject-back'>Issue: {ticket.subject}</p>
+                <p className='ticket-item' id='appt-date-time-back'>Appointment: {apptDate} {ticket.appt_time}</p>
+                <p className='ticket-item' id='ticket-desc'>Description: {ticket.body}</p>
                 <button onClick={this.tenantHandleStatus} className='status-btn'>{status}</button>
-              </ul>
+              </div>
             </div>
             </>
         )
       })
-      return showTickets
     }
   }
 
   render() {
+    console.log(this.state)
 
     if(this.state.ticketModalOpen) {
       return(
@@ -82,14 +91,18 @@ class Tickets extends Component {
           <div onClick={this.handleModalOpen} className='modal-container'>
             <div className='ticket-window-container'>
               {this.displayUnresolvedTickets()}
+              <button onClick={this.handleCreateTicketBtn} className='create-ticket-btn'>Create New Ticket</button>
             </div>
           </div>
         </>
       )
     }
+
     return(
       <>
       <button className='tickets-btn' onClick={this.handleModalOpen}>Tickets</button>
+      <CreateTicketForm
+        createTicket={this.state.creatingTicket} />
       </>
     )
   }
