2f920ca3fff8d4b47d6ccb6e90820dc7c4dcfdd9
Erick <erickarellano@pursuit.org>
2019-04-30 15:04:46 -0400

    component for adding apartment added

---
+++ backend/client/src/TenantDash/searchApartment.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class searchApartment extends Component {
+
+    render() {
+        return (
+            <h1>Add tenant apartment</h1>
+        )
+    }
+}
+
+export default searchApartment;
