1dfac2181ac40d7c8bcc8ebffd2358cf5b3db007
pixelynx <tjstevens1992@gmail.com>
2019-04-30 15:07:45 -0400

    stuck on PUT

--- backend/client/src/TenantDash/tickets.jsx
+++ backend/client/src/TenantDash/tickets.jsx
@@ -13,6 +13,27 @@ class Tickets extends Component {
   componentDidMount = () => {
   }
 
+  tenantHandleStatus = async(e) => {
+    const { defaultValue, user } = this.props
+    e.preventDefault();
+
+    let id = e.target.id;
+    await defaultValue.map(ticket => {
+    axios.put(`/tickets/${id}`, {
+      ticketid: id,
+      apartment_id: user.aptid,
+      completed_tenant: `${ticket.completed_tenant === '1' ? '0' : '1'}`,
+      completed_landlord: ticket.completed_landlord,
+      in_progress: ticket.in_progress,
+      appt_date: ticket.appt_date,
+      appt_time: ticket.appt_time
+    })
+    .then(res => {
+      this.setState(prevState => ({ completed_tenant: !prevState.completed_tenant }))
+    }).catch(err => console.log("put request: ", err))
+    })
+  }
+
 
   handleCreateTicketBtn = (e) => {
     this.setState({ creatingTicket: true })
@@ -30,23 +51,21 @@ class Tickets extends Component {
 
 
   render() {
-    const { completed_tenant } = this.state
+    const { defaultValue } = this.props
     const { ticket } = this.props
-    console.log(this.state)
+    // console.log(completed_tenant, 'defaultvalue PROPS')
 
-      let status = 'URESOLVED';
       let date = ticket.appt_date
       let apptDate = new Intl.DateTimeFormat('en-US').format(new Date(date))
       if(!this.state.hovered) {
         return(
           <>
             <div
-              key={ticket.id}
               className='ticket-front'
               onMouseEnter={this.mouseEnter}>
               <p className='ticket-item' id='ticket-subject-front'>Issue: {ticket.subject}</p>
               <p className='ticket-item' id='appt-date-time-front'>Appointment: {apptDate} {ticket.appt_time}</p>
-              <p>{completed_tenant ? 'UNRESOLVED' : 'RESOLVED'}</p>
+              <p>{ticket.completed_tenant === '0' ? 'UNRESOLVED' : 'RESOLVED'}</p>
             </div>
             </>
         )
@@ -54,16 +73,15 @@ class Tickets extends Component {
             return (
               <>
               <div
-                key={ticket.id}
                 className='ticket-back'
                 onMouseLeave={this.mouseLeave}>
                 <p className='ticket-item' id='ticket-subject-back'>Issue: {ticket.subject}</p>
                 <p className='ticket-item' id='appt-date-time-back'>Appointment: {apptDate} {ticket.appt_time}</p>
                 <p className='ticket-item' id='ticket-desc'>Description: {ticket.body}</p>
               <button
-                id={ticket.id}
+                id={ticket.ticketid}
                 onClick={this.tenantHandleStatus}
-                className='status-btn'>{status}</button>
+                className='status-btn'>{ticket.completed_tenant === '0' ? 'UNRESOLVED' : 'RESOLVED'}</button>
             </div>
           </>
         )
