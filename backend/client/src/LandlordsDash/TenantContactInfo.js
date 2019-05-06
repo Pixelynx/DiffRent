5629c8171292f4219cf99e083404baf701a796dd
pixelynx <tjstevens1992@gmail.com>
2019-05-06 18:33:55 -0400

    css

--- backend/client/src/LandlordsDash/TenantContactInfo.js
+++ backend/client/src/LandlordsDash/TenantContactInfo.js
@@ -10,19 +10,18 @@ if(props.selectedApt) {
 
     return(
       <>
-
-      <div
-      className={props.tenantModalShowing ? 'show-modal' : 'hide-modal'}
-      onClick={props.modalShowing}>
-      <button onClick={props.closeModal} className='close-modal-btn'>x</button>
-        <div className='tenant-info'>
-          <p className='tenants-contact-name'>Name: {selectedTenant.name}</p>
-          <p className='tenants-contact-address'>Address: {selectedTenant.address}</p>
-          <p className='tenants-contact-aptNum'>Apt #: {selectedTenant.apartment_id}</p>
-          <p className='tenants-contact-phone'>Phone: {selectedTenant.phone}</p>
-          <p className='tenants-contact-email'>Email: {selectedTenant.email}</p>
+        <div
+        className={props.tenantModalShowing ? 'show-modal' : 'hide-modal'}
+        onClick={props.modalShowing}>
+        <button onClick={props.closeModal} className='close-modal-btn'>x</button>
+          <div className='tenant-info'>
+            <p className='tenants-contact-name'>Name: {selectedTenant.name}</p>
+            <p className='tenants-contact-address'>Address: {selectedTenant.address}</p>
+            <p className='tenants-contact-aptNum'>Apt #: {selectedTenant.apartment_id}</p>
+            <p className='tenants-contact-phone'>Phone: {selectedTenant.phone}</p>
+            <p className='tenants-contact-email'>Email: {selectedTenant.email}</p>
+          </div>
         </div>
-      </div>
       </>
   )
 } return null
