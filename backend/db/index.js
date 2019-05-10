2ed89084b1967cb93960059bd1603edb9b41e163
pixelynx <tjstevens1992@gmail.com>
2019-05-10 18:40:14 -0400

    connected db

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,4 +1,4 @@
 const pgp = require('pg-promise')({})
-const db = pgp('postgres://localhost:5432/diffrent')
+const db = pgp(process.env.DATABASE_URL || 'postgres://localhost:5432/diffrent')
 
 module.exports = db
