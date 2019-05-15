67b918b4e43e28adbb3113a20d2810b50cb58f1d
pixelynx <tjstevens1992@gmail.com>
2019-05-15 13:53:43 -0400

    archive works. in progress does not update

--- backend/client/src/archivedTickets.jsx
+++ backend/client/src/archivedTickets.jsx
@@ -2,42 +2,20 @@ import React, { Component } from 'react';
 
 class ArchivedTickets extends Component {
   state = {
-    resolvedTickets: []
-  }
-
-  handleResolvedTicket = (defaultValue) => {
-    let resolvedTickets = []
-    return defaultValue.filter(ticket => {
-      if(ticket.completed_tenant === '1' && ticket.completed_landlord === '1') {
-        return resolvedTickets.push(ticket)
-      }
-    })
-    this.setState({ resolvedTickets: resolvedTickets })
   }
 
   render() {
-    const { archivedTicketsShowing, ticket, defaultValue } = this.props;
-
+    const { archiveShowing, ticket, defaultValue } = this.props;
     let date = ticket.appt_date
     let apptDate = new Intl.DateTimeFormat('en-US').format(new Date(date))
 
     return(
       <>
-      { archivedTicketsShowing ?
-
-            <div className='arch-tik-mdl-ctn' onClick={this.handleTicketModal}>
-              <div
-                className='arch-mdl-indv-tik'>
-                <div className='arch-indv-tik'>
-                  <p className='arch-mdl-tik-item' id='arch-tik-subj'>Issue: {ticket.subject}</p>
-                  <p className='arch-mdl-tik-item' id='arch-tik-appt-date-time'>Appointment: {apptDate} {ticket.appt_time}</p>
-                  <p className='arch-mdl-tik-item' id='arch-tik-desc'>Description: {ticket.body}</p>
-                  <p className='resolved'>RESOLVED</p>
-                </div>
-              </div>
-            </div>
-          : null }
-          </>
+        <button onClick={this.handleTicketModal} className='td-tik' id={ticket.ticketid}>
+          <div id={ticket.ticketid} className='td-tik-subject'>{ticket.subject}</div>
+          <div id={ticket.ticketid} className='td-tik-td-appt-dt-tm'>{ticket.appt_date !== null ? apptDate : null} {ticket.appt_time !== null ? ticket.appt_time : null}</div>
+        </button>
+      </>
     )
   }
 }
